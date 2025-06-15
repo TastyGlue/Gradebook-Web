f67699cda341bb57ef6b82a4cea2f11ae5cee5e0
Ivaylo H <ivcony10@gmail.com>
2025-06-15 03:45:42 +0300

    Added School and Teacher forms + Edit/Create.

---
+++ src/Gradebook/Gradebook.Web/Components/Pages/Administator/ManageSchools/CreateSchool.razor.cs
@@ -0,0 +1,33 @@
+﻿namespace Gradebook.Web.Components.Pages.Administator.ManageSchools
+{
+    public partial class CreateSchoolBase : ComponentBase
+    {
+        protected SchoolViewModel School { get; set; } = new();
+
+        protected MudForm _form = default!;
+
+        [Inject] protected NavigationManager Navigation { get; set; } = default!;
+        [Inject] protected ISnackbar Snackbar { get; set; } = default!;
+
+        protected async Task SubmitAsync()
+        {
+            await _form.Validate();
+
+            if (_form.IsValid)
+            {
+                // TODO: Save logic (via service)
+                Snackbar.Add("School created successfully!", Severity.Success);
+                Navigation.NavigateTo("/manageschools");
+            }
+            else
+            {
+                Snackbar.Add("Please fill all required fields.", Severity.Warning);
+            }
+        }
+
+        protected void GoBack()
+        {
+            Navigation.NavigateTo("/manageschools");
+        }
+    }
+}
