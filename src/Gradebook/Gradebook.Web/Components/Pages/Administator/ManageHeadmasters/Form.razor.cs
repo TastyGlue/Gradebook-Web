3c6aaeee5a40dc70e844d42f4e8ac48af55b66c1
Teodor Georgiev <sitikom2007@gmail.com>
2025-06-16 20:14:43 +0300

    Refactor headmaster management functionality
    Updated HeadMasterService to include async retrieval methods for headmasters. Modified CreateHeadmaster UI to use a new ViewModel and added school selection. Refactored ManageHeadmasters to utilize the new ApiHeadmasterService. Replaced old SchoolViewModelche with SchoolViewModel for consistency. Introduced Form components for encapsulating headmaster logic. Enhanced validation in ViewModel classes and improved API interaction methods. Overall, these changes enhance code organization, readability, and maintainability.

---
+++ src/Gradebook/Gradebook.Web/Components/Pages/Administator/ManageHeadmasters/Form.razor.cs
@@ -0,0 +1,37 @@
+﻿namespace Gradebook.Web.Components.Pages.Administator.ManageHeadmasters;
+
+public partial class Form : ExtendedComponentBase
+{
+    [Parameter] public HeadmasterViewModel ViewModel { get; set; } = new();
+    [Parameter] public new string PageTitle { get; set; } = default!;
+    [Parameter] public EventCallback OnValidSubmit { get; set; }
+
+    [Inject] protected IApiSchoolService ApiSchoolService { get; set; } = default!;
+
+    protected IEnumerable<SchoolViewModel> Schools { get; set; } = [];
+
+    protected override async Task OnInitializedAsync()
+    {
+        await LoadSchools();
+    }
+
+    protected async Task ValidSubmitHandler()
+        => await OnValidSubmit.InvokeAsync();
+
+    protected async Task LoadSchools()
+    {
+        var result = await ApiSchoolService.GetSchools();
+        if (result.Succeeded)
+        {
+            Schools = result.Value!.Adapt<List<SchoolViewModel>>();
+        }
+        else
+        {
+            Notify(result.Error!.Message, Severity.Error);
+            NavigationManager.NavigateTo("/");
+        }
+    }
+
+    protected void CancelHandler()
+        => NavigationManager.NavigateTo("manage-headmasters");
+}
