33ca8af9cc0ab2a2dd799e15a6d0b110b1e2e2f5
Teodor Georgiev <sitikom2007@gmail.com>
2025-07-07 21:44:09 +0300

    Added AbsenceDialog

---
+++ src/Gradebook/Gradebook.Web/Components/Shared/AbsenceDialogForm.razor.cs
@@ -0,0 +1,39 @@
+﻿namespace Gradebook.Web.Components.Shared;
+
+public partial class AbsenceDialogForm : ExtendedComponentBase
+{
+    [CascadingParameter]
+    private IMudDialogInstance MudDialog { get; set; } = default!;
+
+    [Parameter]
+    public TimetableViewModel Timetable { get; set; } = default!;
+
+    [Parameter]
+    public IEnumerable<StudentViewModel> Students { get; set; } = [];
+
+    protected MudForm FormRef { get; set; } = default!;
+
+    protected async Task SubmitHandler()
+    {
+        await FormRef.Validate();
+
+        if (FormRef.IsValid)
+        {
+            // Logic to add absence
+
+            bool result = true; // Replace with actual logic to add absence
+
+            if (result) 
+            {
+                Notify("Absence added successfully.", Severity.Success);
+                MudDialog.Close(DialogResult.Ok(true));
+            }
+            else
+            {
+                Notify("Failed to add absence.", Severity.Error);
+            }
+        }
+    }
+
+    private void CancelHandler() => MudDialog.Cancel();
+}
